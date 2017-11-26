8854785f872142201639e52c4fc60e7097c17b84
yugin <>
2017-11-26 16:15:02 +0300

    установлен opencart Version 2.1.0.1 (rs.2)

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://store/admin/');
+define('HTTP_CATALOG', 'http://store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://store/admin/');
+define('HTTPS_CATALOG', 'http://store/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OSPanel/domains/store/admin/');
+define('DIR_SYSTEM', 'C:/OSPanel/domains/store/system/');
+define('DIR_LANGUAGE', 'C:/OSPanel/domains/store/admin/language/');
+define('DIR_TEMPLATE', 'C:/OSPanel/domains/store/admin/view/template/');
+define('DIR_CONFIG', 'C:/OSPanel/domains/store/system/config/');
+define('DIR_IMAGE', 'C:/OSPanel/domains/store/image/');
+define('DIR_CACHE', 'C:/OSPanel/domains/store/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OSPanel/domains/store/system/storage/download/');
+define('DIR_LOGS', 'C:/OSPanel/domains/store/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OSPanel/domains/store/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OSPanel/domains/store/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OSPanel/domains/store/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'akkond');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
